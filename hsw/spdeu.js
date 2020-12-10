2648af365de97518da37696d261e3188d1cfd11c
Penk1 <75736461+Penk1@users.noreply.github.com>
2020-12-10 10:10:15 +0100

    Add files via upload

---
+++ hsw/spdeu.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  
+
+
+  $('.text').click(function(){
+      $(this).find('.text-p').toggle(3);
+      $(this).find('.span-wrapper').toggle(3);
+    });
+
+
+    $('button').mouseenter(function(){
+      $('.destroy').hide();
+      $('#blog').fadeIn();
+    });
+
+    
+
+
+   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
