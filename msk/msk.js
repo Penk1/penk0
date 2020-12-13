3fe6bc7f286ae18d6ba303b96f515a32618f69d0
Penk1 <75736461+Penk1@users.noreply.github.com>
2020-12-13 16:04:45 +0100

    Update msk.js

--- msk/msk.js
+++ msk/msk.js
@@ -6,4 +6,13 @@ $(function(){
      $('#penktv').fadeIn(5).css('background-color','grey').fadeOut(100);
    });
 
+   $('#button2').click(function(){
+    $('.maintainance').fadeIn();
+  });
+
+  $('#button2').mouseleave(function(){
+    $('.maintainance').fadeOut();
+  })
+      
+
  });
