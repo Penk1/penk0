346847525509d17eed1e1985699e78fbc72611af
Penk1 <75736461+Penk1@users.noreply.github.com>
2020-12-16 17:21:18 +0100

    Update alk.js

--- alk/alk.js
+++ alk/alk.js
@@ -1,16 +1,31 @@
 $(function(){
-
+  $('#blog').hide();
 
 $('.categry').click(function(){
   
   $(this).find('.text').toggle(50);
 
 
+});
 
+$('.logo').mouseenter(function(){
+  $('#close').fadeIn(500).fadeOut(500);
 
 });
 
 
+
+
+$('.chaos').hover(function(){
+  $('#chaos1').hide();
+  $('#blog').fadeIn(500);
+});
+
+
+
+
+
+});
 
 
 
